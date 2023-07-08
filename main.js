c6d962b19cdb0a45222db0a822d3777efe1f9e21
闭才 刘 <liubicai@outlook.com>
2023-07-08 16:06:24 +0800

    为渲染器添加需要特殊权限的功能，可以通过 contextBridge 接口定义 全局对象。

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
+    height: 600,
+     webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+      //__dirname 字符串指向当前正在执行的脚本的路径(在本例中，它指向你的项目的根文件夹)。
+      //path.join API 将多个路径联结在一起，创建一个跨平台的路径字符串。
+    }
   })
 
   win.loadFile('index.html')
