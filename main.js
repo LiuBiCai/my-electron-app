4d91d2352e3c5fedec201ca47d078caabe31d531
闭才 刘 <liubicai@outlook.com>
2023-07-08 16:16:28 +0800

    在进程之间通信

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 const createWindow = () => {
@@ -17,6 +17,7 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 
   app.on('activate', () => {
